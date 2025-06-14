72f86336f5ff9666df63f7c9d5b0c67f901a6e16
0neAnd0nlyKen <kendrickraphael@gmail.com>
2025-06-14 18:34:19 +0800

    golang package main test

---
+++ api/main.go
@@ -0,0 +1,12 @@
+package main
+
+//import local package handler
+import (
+	"net/http"
+)
+
+func main() {
+	r := http.NewServeMux()
+	r.HandleFunc("GET /", HomeHandler)
+	http.ListenAndServe(":8080", r)
+}
